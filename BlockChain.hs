cb15bc47b55ab566c158e5e7a57cb34a98f26277
Saurabh Kukade <saurabhkukade@gmail.com>
2020-01-10 11:09:23 +0530

    Added serialization logic

--- BlockChain.hs
+++ BlockChain.hs
@@ -1,88 +1,102 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module BlockChain where
-import Data.Time.Clock
 
-import Crypto.Hash
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.UTF8 as UTF8
-import Data.ByteArray.Encoding
-
+import           Data.Time.Clock
+import           Crypto.Hash
+import           Data.ByteString                ( ByteString )
+import qualified Data.ByteString.UTF8          as UTF8
+import           Data.ByteArray.Encoding
+import           Data.Binary
+import           GHC.Generics                   ( Generic )
 
 
 type Index = Int
 type Difficulty = Int
 type PrevHash = String
-type Timestamp = UTCTime
+type Timestamp = String
 type BlockData = String
 type Hash = String
 type Nonce = Int
 type BlockChain = [Block]
 
 
-data Block = Block { index :: Index
-                    ,prevHash :: PrevHash
-                    ,timestamp :: Timestamp
-                    ,blockData :: BlockData
-                    ,blockHash :: Hash
-                    ,nonce :: Nonce
-                    ,difficulty :: Difficulty
-                   } deriving (Show, Eq)
+data Block = Block
+    { index      :: Index
+    , prevHash   :: PrevHash
+    , timestamp  :: Timestamp
+    , blockData  :: BlockData
+    , blockHash  :: Hash
+    , nonce      :: Nonce
+    , difficulty :: Difficulty
+    }
+    deriving (Show, Eq, Generic)
+
+
+instance Binary Block
 
 
 sha256 :: String -> String
 sha256 input = result
   where
-    bytes = UTF8.fromString input      :: ByteString
-    digest = hashWith SHA256 bytes     :: Digest SHA256
-    hex = convertToBase Base16 digest  :: ByteString
-    result = UTF8.toString hex         :: String
+    bytes  = UTF8.fromString input :: ByteString
+    digest = hashWith SHA256 bytes :: Digest SHA256
+    hex    = convertToBase Base16 digest :: ByteString
+    result = UTF8.toString hex :: String
 
 
 calculateHashForBlock :: Block -> (String, Nonce)
-calculateHashForBlock block = calculateHashForBlock' (index block) (prevHash block) (timestamp block) (blockData block) (nonce block) (difficulty block)
+calculateHashForBlock block = calculateHashForBlock' (index block)
+                                                     (prevHash block)
+                                                     (timestamp block)
+                                                     (blockData block)
+                                                     (nonce block)
+                                                     (difficulty block)
 
 
 calculateHashForBlock' :: Index -> PrevHash -> Timestamp -> BlockData -> Nonce -> Difficulty -> (String, Nonce)
-calculateHashForBlock' index prevHash timestamp blockData nonce difficulty = 
-    let hash = createHash (hashData nonce) in
-        if (isValidHashDifficulty hash difficulty) then (hash, nonce)
-                                      else calculateHashForBlock' index prevHash timestamp blockData (nonce + 1) difficulty
-    where
-      hashData nonce' =  (show nonce') ++ (show index) ++ (show prevHash) ++ (show timestamp) ++ blockData
-      createHash sdata = sha256 sdata
+calculateHashForBlock' index prevHash timestamp blockData nonce difficulty =
+    let hash = createHash (hashData nonce)
+    in  if (isValidHashDifficulty hash difficulty)
+            then (hash, nonce)
+            else calculateHashForBlock' index prevHash timestamp blockData (nonce + 1) difficulty
+  where
+    hashData nonce' = (show nonce') ++ (show index) ++ (show prevHash) ++ (show timestamp) ++ blockData
+    createHash sdata = sha256 sdata
 
 
 generateNextBlock :: Block -> BlockData -> IO Block
 generateNextBlock currentBlock bdata = do
     timestamp <- getCurrentTime
-    let nextIndex = (index currentBlock) + 1
-    let prevHash = blockHash currentBlock
-    let nonce = 0
-    let hashDifficulty = difficulty currentBlock
-    let (nextHash,uNonce) = calculateHashForBlock' nextIndex prevHash timestamp bdata nonce hashDifficulty 
-    return (Block nextIndex prevHash timestamp bdata nextHash uNonce hashDifficulty)
+    let nextIndex          = (index currentBlock) + 1
+    let prevHash           = blockHash currentBlock
+    let nonce              = 0
+    let hashDifficulty     = difficulty currentBlock
+    let (nextHash, uNonce) = calculateHashForBlock' nextIndex prevHash (show timestamp) bdata nonce hashDifficulty
+    return (Block nextIndex prevHash (show timestamp) bdata nextHash uNonce hashDifficulty)
 
 
 isValidNextBlock :: Block -> Block -> Bool
-isValidNextBlock currentBlock newBlock = let (newBlockHash, nonce) = (calculateHashForBlock newBlock) in
-                                            validateNextBlock currentBlock newBlock newBlockHash
-      where
-        validateNextBlock cBlock nBlock newBlockHash
-          | (blockHash cBlock) /= (prevHash nBlock) = False
-          | (index cBlock) + 1 /= (index nBlock) = False
-          | newBlockHash /= (blockHash nBlock) = False
-          | otherwise = True
+isValidNextBlock currentBlock newBlock =
+    let (newBlockHash, nonce) = (calculateHashForBlock newBlock)
+    in  validateNextBlock currentBlock newBlock newBlockHash
+  where
+    validateNextBlock cBlock nBlock newBlockHash | (blockHash cBlock) /= (prevHash nBlock) = False
+                                                 | (index cBlock) + 1 /= (index nBlock)    = False
+                                                 | newBlockHash /= (blockHash nBlock)      = False
+                                                 | otherwise                               = True
 
 
 isValidHashDifficulty :: Hash -> Int -> Bool
-isValidHashDifficulty hash df = (length $ takeWhile (=='0') hash) >= df
+isValidHashDifficulty hash df = (length $ takeWhile (== '0') hash) >= df
 
 
 isValidBlockChain :: BlockChain -> Bool
-isValidBlockChain (b:bs) = isValidBlockChain' b bs
-    where
-      isValidBlockChain' _ [] = True
-      isValidBlockChain' b0 (b1:bs) | (isValidNextBlock b0 b1) = isValidBlockChain' b1 bs
-                                    | otherwise = False
+isValidBlockChain (b : bs) = isValidBlockChain' b bs
+  where
+    isValidBlockChain' _ [] = True
+    isValidBlockChain' b0 (b1 : bs) | (isValidNextBlock b0 b1) = isValidBlockChain' b1 bs
+                                    | otherwise                = False
 
 isChainLonger :: BlockChain -> BlockChain -> Bool
 isChainLonger currentBlockChain newBlockChain = length currentBlockChain > length newBlockChain
@@ -91,22 +105,28 @@ isChainLonger currentBlockChain newBlockChain = length currentBlockChain > lengt
 genesisBlock :: IO Block
 genesisBlock = do
     now <- getCurrentTime
-    let prevHash = "0"
-    let blockData = "Welcome to demo blockchain"
-    let difficulty = 2
-    let (hash, uNonce) = calculateHashForBlock' 1 prevHash now blockData 0 difficulty
-    return (Block 1 prevHash now blockData hash uNonce difficulty)
+    let prevHash       = "0"
+    let blockData      = "Welcome to demo blockchain"
+    let difficulty     = 2
+    let (hash, uNonce) = calculateHashForBlock' 1 prevHash (show now) blockData 0 difficulty
+    return (Block 1 prevHash (show now) blockData hash uNonce difficulty)
 
 
 mine :: BlockChain -> BlockData -> IO (Maybe BlockChain)
-mine  blockChain blockData = do
+mine blockChain blockData = do
     let currentBlock = (last blockChain)
     newBlock <- generateNextBlock currentBlock blockData
     return (validateNewBlock currentBlock newBlock)
-      where
-        validateNewBlock currentBlock newBlock | (isValidNextBlock currentBlock newBlock) = Just (blockChain ++ [newBlock])
-                                 | otherwise = Nothing
-                                     
+  where
+    validateNewBlock currentBlock newBlock | (isValidNextBlock currentBlock newBlock) = Just (blockChain ++ [newBlock])
+                                           | otherwise = Nothing
+
+
+getSerializedBlockChain chain = Data.Binary.encode chain
+getDeserializedBlockChain chain = Data.Binary.decode chain :: Maybe BlockChain
+
+
+
 initBlockChain :: IO BlockChain
 initBlockChain = do
     genBlock <- genesisBlock
@@ -116,6 +136,6 @@ initBlockChain = do
 run = do
     b1 <- genesisBlock
     b2 <- generateNextBlock b1 "data_1"
-    let chain = [b1,b2]
+    let chain = [b1, b2]
     newChain <- mine chain "data_3"
     return newChain
